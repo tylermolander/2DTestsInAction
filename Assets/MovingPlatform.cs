d09e62a5eb8d82c28f5b08582db3fe9580fd0b2e
Tyler Molander <tylermolander@gmail.com>
2018-09-03 21:58:01 -0400

    TestStart

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public Vector3 finishPos = Vector3.zero;
+    public float speed = 0.5f;
+
+    Vector3 startPos;
+    float trackPercent = 0;  //how far between start and finish
+    int direction = 1;
+
+
+	void Start () 
+	{
+		startPos = transform.position;
+	}
+	
+	void Update ()
+	{
+	    trackPercent += direction * speed * Time.deltaTime;
+	    float x = (finishPos.x - startPos.x) * trackPercent * startPos.x;
+	    float y = (finishPos.y - startPos.y) * trackPercent * startPos.y;
+	    transform.position = new Vector3(x, y, startPos.z);
+	}
+}
---
+++ Assets/PlatformerConroller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerConroller : MonoBehaviour {
+
+    public float speed = 250f;
+    public float jumpForce = 12f;
+    private Rigidbody2D myRigidBody;
+    private BoxCollider2D boxCollider;
+
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Vector2 movement = new Vector2(deltaX, myRigidBody.velocity.y);
+        myRigidBody.velocity = movement;
+
+
+        Vector3 max = boxCollider.bounds.max;
+        Vector3 min = boxCollider.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - .1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - .2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+
+        bool grounded = false;
+
+        if(hit != null)
+        {
+            grounded = true;
+        }
+
+
+        myRigidBody.gravityScale = grounded && deltaX == 0 ? 0 : 1;
+
+        if (grounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button1) ))
+        {
+            myRigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+	}
+}
